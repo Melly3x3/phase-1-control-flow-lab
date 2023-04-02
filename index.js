610505fe8641825c7de98b0ca4352644b7d7e6f5
Melvin <jmelvin1134@gmail.com>
2023-04-02 18:30:27 -0400

    ctrl flow lab complete

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting
+if(distance <= 400) {
+  greeting = "This one is on me!";
+  return greeting ; 
+}
+else if (distance >= 400 && distance <= 2000) {
+  greeting = 'That will be twenty bucks.'
+  return greeting
+}
+else if (distance >= 2000 && distance <= 2500) {
+  greeting = 'I will gladly take your thirty bucks.'
+  return greeting
+}
+else if(distance > 2500){
+  greeting = 'No can do.'
+  return greeting
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = city === `NYC` ? `Ok, sounds good.` : `No go.`
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return ("Thank you so much.");
+    case "not as generous":
+      return ("Thank you.");
+    default:
+      return ("Bye.");
+  }
 }
